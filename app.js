51b211bc39954b713784f2d89580b4356a22b759
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-07-03 16:58:30 +0700

    ended preload

---
+++ app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    setTimeout(()=>{
+        const preloader = document.querySelector('.preload');
+        preloader.classList.add("preload-finish");
+    }, 1700);
+   
+});
